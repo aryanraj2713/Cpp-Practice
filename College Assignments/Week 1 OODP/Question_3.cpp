02839e61c3b88ff99c81bcf380f5c696fbc11116
Aryan <aryanraj2713@gmail.com>
2022-09-05 16:38:56 +0530

    Assignment

---
+++ College Assignments/Week 1 OODP/Question_3.cpp
@@ -0,0 +1,28 @@
+
+
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+bool isPrime(int num){
+    for(int i=2 ; i<=sqrt(num) ; i++){
+        if( num%2==0 ){
+            return false;
+        }
+
+    }
+    return true;                      
+}
+
+int main()
+{
+    int a , b ;
+    cin >> a >> b ;
+    for(int i = a ; i <= b ; i++){
+        if(isPrime(i)==true){           .     
+            cout << i << endl ;
+        }
+    }
+    return 0;
+}

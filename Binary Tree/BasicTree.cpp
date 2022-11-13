df5c989b15e349e21941122df8c770e4a14ac3e3
Aryan <aryanraj2713@gmail.com>
2022-11-13 21:35:22 +0530

    Trees

---
+++ Binary Tree/BasicTree.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    int data ;
+    struct Node* left ;
+    struct Node* right ;
+
+    //constructor
+    Node(int val){
+        data = val ;
+        left = NULL ;
+        right = NULL ;
+    }
+}
+
+int main()
+{   
+    struct Node* root = new Node(1) ;
+    root -> right = new Node(2) ;
+    root -> left = now Node(3) ;
+    //basic tree-IWT to expand my treeeee
+    root -> left -> left = new Node(5);
+    root -> left -> right = new Node (6);
+    
+    return 0;
+}

34fe6929b510c1c63ba58754b293dc9bbe95511e
Aryan <aryanraj2713@gmail.com>
2022-11-27 22:00:58 +0530

    changes

---
+++ Some basic important Questions/elab_trick.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class mandatort_test{
+    mandatort_test(){
+        string mandatory = "mandatory test_string";
+    }
+    
+    };
+int main() {
+    if(5){
+    cout<<"10";
+    }
+
+return 0 ;
+    
+
+}

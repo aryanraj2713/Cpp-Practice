5def9116435575b53b52da6a6f3ba9c9e7a5674f
Aryan <aryanraj2713@gmail.com>
2022-11-28 12:36:21 +0530

    STL

---
+++ Some basic important Questions/elab_trick2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class mandatort_test{
+    mandatort_test(){
+        string mandatory = "mandatory test_string";
+    }
+    
+    };
+int main() {
+    //Assume some queue question
+    //use array 
+    //output like 
+    // 2 Enqueue 3
+    // 4 Enqueue 5 
+    // ----
+    // ---
+     int arr[100] ;
+     //setup loop
+     cout << arr[i-1 ] << "Enqueue" << arr[i] << endl;
+
+return 0 ;
+    
+
+}
--- cppSTL/vectorStl.cpp
+++ cppSTL/vectorStl.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int main()
 {
     vector<int> v ;
+    for (int i = 0; i < 500; i++)
+    {
+        v.push_back(i);
+    }
+
+    for (int i = 0; i < 500; i++)
+    {
+        cout << v[i] << endl;
+    }
+    
     
     return 0;
 }
